9277afc9d99b27ca508d0ff2ddabb4c80066f92c
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-10 12:20:03 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_Basic_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Basic_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Иванов ";
+            string lastName = "Саша ";
+
+            Console.WriteLine("Имя - " + firstName + " Фамилия - " + lastName);
+
+            string realFirstName = lastName;
+            string realLastName = firstName;
+
+            Console.WriteLine("Имя - " + realFirstName + " Фамилия - " + realLastName);
+
+        }
+    }
+}
